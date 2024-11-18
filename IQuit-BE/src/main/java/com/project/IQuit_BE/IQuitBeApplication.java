22448f45b0dfc9ef13c24c443b9d16083562ef67
clkbrr <clkberraa@gmail.com>
2024-11-18 08:26:53 +0300

    first commit

---
+++ IQuit-BE/src/main/java/com/project/IQuit_BE/IQuitBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.IQuit_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IQuitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IQuitBeApplication.class, args);
+	}
+
+}
---
+++ IQuit-BE/src/test/java/com/project/IQuit_BE/IQuitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.IQuit_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IQuitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
